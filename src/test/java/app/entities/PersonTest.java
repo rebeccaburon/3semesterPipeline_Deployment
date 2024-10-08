500314a6ee1f1c7056c0a5b3bd24be72da721b3c
rebeccaburon <rebecca.mary.buron@hotmail.com>
2024-10-08 13:46:48 +0200

    Adding test

--- src/test/java/app/entities/PersonTest.java
+++ src/test/java/app/entities/PersonTest.java
@@ -20,30 +20,4 @@ class PersonTest {
 
     }
 
-    @Test
-    void forTest2() {
-        Person person = new Person();
-
-        int expected = 72;
-
-        int actual = person.ForTest1();
-
-        assertEquals(expected, actual);
-
-
-    }
-
-    @Test
-    void forTest3() {
-        Person person = new Person();
-
-        int expected = 4;
-
-        int actual = person.ForTest1();
-
-        assertEquals(expected, actual);
-
-
-    }
-
 }
